2986a047dbf9f642b45c7c18dcea53d21c4b5862
Dmitry Ananichev <a@qozz.ru>
2017-08-14 17:48:38 +0500

    run command update

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker run --name litecoin-testbox -tid -p 19001:19001 cryptochain/litecoind-docker:latest \
+docker run --name litecoin-testbox -tid \
+  -p 9332:9332 \
+  -p 9333:9333 \
+  -p 19332:19332 \
+  -p 19333:19333 \
+  -p 19444:19444 \
+  cryptochain/litecoind-docker:latest \
   --printtoconsole \
   -regtest=1 \
   -rpcallowip=0.0.0.0/0 \
