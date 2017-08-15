b3b752c253e651f8e07ee7bf1c0bec176fda36de
Dmitry Ananichev <a@qozz.ru>
2017-08-15 20:10:42 +0500

    custom port for rpcservice and server enabled

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 docker run --name litecoin-regtest -tid \
-  -p 9332:9332 \
-  -p 9333:9333 \
   -p 19332:19332 \
   -p 19333:19333 \
-  -p 19444:19444 \
   cryptochain/litecoind-docker:latest \
   --printtoconsole \
+  -server \
+  -port=19333 \
   -regtest=1 \
   -rpcallowip=0.0.0.0/0 \
   -rpcpassword=admin1 \
-  -rpcuser=123
+  -rpcuser=123 \
+  -rpcport=19332
