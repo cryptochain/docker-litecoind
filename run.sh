aadd72d14d740726f85e3c1bd4cf86ea8afee601
Dmitry Ananichev <a@qozz.ru>
2017-08-15 20:17:11 +0500

    different settings

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 docker run --name litecoin-regtest -tid \
-  -p 19332:19332 \
-  -p 19333:19333 \
+  -p 20001:20001 \
+  -p 20011:20011 \
   cryptochain/litecoind-docker:latest \
   --printtoconsole \
   -server \
-  -port=19333 \
+  -listen \
+  -port=20001 \
   -regtest=1 \
   -rpcallowip=0.0.0.0/0 \
   -rpcpassword=admin1 \
   -rpcuser=123 \
-  -rpcport=19332
+  -rpcport=20011
