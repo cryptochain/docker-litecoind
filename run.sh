b5f8374bc6f6cba0574db5848711a74918f46a89
Dmitry Ananichev <a@qozz.ru>
2018-02-11 21:59:18 +0500

    docker-compose added

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker run --name litecoin-regtest -tid \
+docker run --name litecoin-testnet -tid \
   -p 20001:20001 \
   -p 20011:20011 \
   cryptochain/litecoind-docker:latest \
   --printtoconsole \
   -server=1 \
   -listen=1 \
-  -regtest=1 \
+  -testnet=1 \
   -port=20001 \
   -rpcallowip=0.0.0.0/0 \
   -rpcuser=admin1 \
